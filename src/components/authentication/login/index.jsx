e11851c51c3189b0348cdc24bf45de1c0c44ddba
Nikhil Karakoti <nikhil.karakoti@grazitti.com>
2023-03-26 14:51:27 +0530

    auth page initial commit

--- src/components/authentication/login/index.jsx
+++ src/components/authentication/login/index.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import { Fragment, useState } from "react";
+import "./style.css";
 
-const Login = props => {
+const Login = (props) => {
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [isEmailValid, setEmailValid] = useState(false);
+    const [isPassValid, setPassValid] = useState(false);
+
+    const renderErrorMessage = (name) => {
+        name === errorMessages.name && (
+            <div className="error">{errorMessages.message}</div>
+        );
+    }
+    const handleSubmit = (event) => event.preventDefault();
+
+    function isValidEmail(email) {
+        return /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/.test(email);
+      }
+    
+    const handleEmailChange = event => {
+        if (isValidEmail(event.target.value)) setEmailValid(true);
+    };
+    const handlePassChange = event => {
+        console.log(event.target.value.length)
+        if (event.target.value.length > 8) setPassValid(true);
+    };
+
     return (
-        <div>
-          login
-        </div>
+        <Fragment>
+            <form onSubmit={handleSubmit}>
+                <div className="input-container">
+                    <label>Email </label>
+                    <input type="text" name="email" required onChange={handleEmailChange} />
+                    {renderErrorMessage("email")}
+                </div>
+                <div className="input-container">
+                    <label>Password </label>
+                    <input type="password" name="pass" required  onChange={handlePassChange}/>
+                    {renderErrorMessage("pass")}
+                </div>
+                <div className="button-container">
+                    <button disabled={!isEmailValid && !isPassValid} type="submit">Login</button>
+                </div>
+            </form>
+        </Fragment>
     );
-
-}
+};
 
 export default Login;
